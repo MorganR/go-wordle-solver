7b481d91c413acef0b2488b76176fe209da1fe44
Morgan Roff <roffmorgan@gmail.com>
2022-07-02 17:54:09 +0200

    Add WordBankFromReader

--- lib/word_bank.go
+++ lib/word_bank.go
@@ -1,7 +1,11 @@
 package go_wordle_solver
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // WordBank provides a read-only set of equal length words.
@@ -10,20 +14,58 @@ type WordBank struct {
 	wordLength uint
 }
 
-// Creates a WordBank from the given word list.
+const defaultWordBuffer int = 100
+
+// Constructs a new `WordBank` struct by reading words from the given reader.
 //
-// The list must be non-empty, and the words must all have the same length. Duplicate words are
-// removed.
-func InitWordBank(words []string) (WordBank, error) {
+// The reader should provide one word per line. Each word will be trimmed and converted to
+// lower case. Empty lines are skipped. At least one word must be provided.
+//
+// After trimming, all words must be the same length, else this returns an error.
+func WordBankFromReader(r io.Reader) (WordBank, error) {
+	s := bufio.NewScanner(r)
+	words := make([][]rune, 0, defaultWordBuffer)
+	n := 0
+	wordLength := 0
+	for ok := s.Scan(); ok; ok = s.Scan() {
+		thisWord := []rune(strings.ToLower(strings.TrimSpace(s.Text())))
+		thisWordLength := len(thisWord)
+		if thisWordLength == 0 {
+			continue
+		}
+		words = append(words, thisWord)
+		if n == 0 {
+			wordLength = thisWordLength
+		}
+		if thisWordLength != wordLength {
+			return WordBank{}, fmt.Errorf("Words must all be the same length. Encountered word with length %v when expecting length %v.", thisWordLength, wordLength)
+		}
+		n++
+	}
+	if err := s.Err(); err != nil {
+		return WordBank{}, err
+	}
+	if len(words) == 0 {
+		return WordBank{}, errors.New("At least one word must be provided.")
+	}
+	return WordBank{words[:], uint(wordLength)}, nil
+}
+
+// Constructs a new `WordBank` struct using the words from the given vector.
+//
+// Each word will be trimmed and converted to lower case. At least one word must be provided.
+//
+// After trimming, all words must be the same length, else this returns an error.
+func WordBankFromSlice(words []string) (WordBank, error) {
 	if len(words) == 0 {
 		return WordBank{}, errors.New("At least one word must be provided.")
 	}
 	wordLength := len(words[0])
 	allWords := make([][]rune, len(words))
 	for i, word := range words {
-		wordRunes := []rune(word)
+		wordRunes := []rune(strings.ToLower(strings.TrimSpace(word)))
 		if len(wordRunes) != wordLength {
-			return WordBank{}, errors.New("Words must all be the same length.")
+			return WordBank{}, fmt.Errorf("Words must all be the same length. Encountered word with length %v when expecting length %v.", len(wordRunes), wordLength)
 		}
 		allWords[i] = wordRunes
 	}
--- lib/word_bank_test.go
+++ lib/word_bank_test.go
@@ -1,28 +1,52 @@
 package go_wordle_solver
 
 import (
+	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
 )
 
-func TestInitWordBankWithNilList(t *testing.T) {
-	_, err := InitWordBank(nil)
+func TestWordBankFromReaderWhenEmpty(t *testing.T) {
+	_, err := WordBankFromReader(strings.NewReader(""))
 	assert.Error(t, err, "At least one word must be provided.")
 }
 
-func TestInitWordBankWithEmptyList(t *testing.T) {
-	_, err := InitWordBank([]string{})
+func TestWordBankFromReaderWithOnlyWhiteSpace(t *testing.T) {
+	_, err := WordBankFromReader(strings.NewReader("  \n  "))
 	assert.Error(t, err, "At least one word must be provided.")
 }
 
-func TestInitWordBankWithDifferentLengthWords(t *testing.T) {
-	_, err := InitWordBank([]string{"bad", "rad", "good"})
-	assert.Error(t, err, "Words must all be the same length.")
+func TestWordBankFromReaderDifferentLengthWords(t *testing.T) {
+	_, err := WordBankFromReader(strings.NewReader("abc\nbcd\nefgh"))
+	assert.Error(t, err, "Words must all be the same length. Encountered word with length 4 when expecting length 3.")
+}
+
+func TestWordBankFromReaderValidWords(t *testing.T) {
+	bank, err := WordBankFromReader(strings.NewReader("abc\n bcd \nef£"))
+
+	assert.NilError(t, err)
+	pw := bank.Words()
+	assert.Equal(t, pw.Len(), 3)
+}
+
+func TestWordBankFromSliceWithNilList(t *testing.T) {
+	_, err := WordBankFromSlice(nil)
+	assert.Error(t, err, "At least one word must be provided.")
+}
+
+func TestWordBankFromSliceWithEmptyList(t *testing.T) {
+	_, err := WordBankFromSlice([]string{})
+	assert.Error(t, err, "At least one word must be provided.")
+}
+
+func TestWordBankFromSliceWithDifferentLengthWords(t *testing.T) {
+	_, err := WordBankFromSlice([]string{"bad", "rad", "good"})
+	assert.Error(t, err, "Words must all be the same length. Encountered word with length 4 when expecting length 3.")
 }
 
 func TestWordBankWords(t *testing.T) {
-	bank, _ := InitWordBank([]string{"foo", "bar"})
+	bank, _ := WordBankFromSlice([]string{"foo", "bar"})
 	pw := bank.Words()
 
 	assert.Equal(t, pw.Len(), 2)
